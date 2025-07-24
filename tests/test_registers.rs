4d4c5dde248e77189754bf190cf829bd9b56231e
Emil Kovacev <Emil.Kovacev@ibm.com>
2025-07-24 12:00:51 -0400

    modify register tests

--- tests/test_registers.rs
+++ tests/test_registers.rs
@@ -1,4 +1,8 @@
-use qiskit_rs::{QuantumCircuit,QuantumRegister,ClassicalRegister};
+use qiskit_rs::{
+    QuantumCircuit,
+    QuantumRegister,
+    ClassicalRegister,
+};
 
 #[test]
 fn test_initialize_registers() {
@@ -7,10 +11,15 @@ fn test_initialize_registers() {
 }
 
 #[test]
-fn test_add_registers_to_circuit() {
+fn test_add_quantum_register() {
     let qreg = QuantumRegister::new(2, "qreg");
-    let creg = ClassicalRegister::new(2, "creg");
     let mut qc = QuantumCircuit::new(2, 0);
     qc.add_quantum_register(qreg);
+}
+
+#[test]
+fn test_add_classical_register() {
+    let creg = ClassicalRegister::new(2, "creg");
+    let mut qc = QuantumCircuit::new(2, 0);
     qc.add_classical_register(creg);
 }
