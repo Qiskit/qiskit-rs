0a5c35f46965b033f66db6b20826b558033ade42
Emil Kovacev <Emil.Kovacev@ibm.com>
2025-09-13 15:00:48 -0400

    Add environment variable options for building the repo

--- build.rs
+++ build.rs
@@ -1,38 +1,90 @@
-use git2::Repository;
+use git2;
 use std::path::Path;
 use std::process::Command;
-use std::io::{self, Write};
+use std::env;
 
-fn build_qiskit() {
-    
+enum InstallMethod {
+    SOURCE(String),
+    BIN(String)
 }
 
-fn main() {
+fn check_installation_method() -> InstallMethod {
+    let qiskit_rs_source_install: Option<String> = match env::var("QISKIT_RS_SOURCE_INSTALL") {
+        Ok(val) => Some(val),
+        Err(e) => match e {
+            env::VarError::NotPresent => None,
+            env::VarError::NotUnicode(env) => panic!("Envvar QISKIT_RS_SOURCE_INSTALL is not unicode: {env:?}")
+        }
+    };
+
+    let qiskit_rs_binary_install: Option<String> = match env::var("QISKIT_RS_BIN_INSTALL") {
+        Ok(val) => Some(val),
+        Err(e) => match e {
+            env::VarError::NotPresent => None,
+            env::VarError::NotUnicode(env) => panic!("Envvar QISKIT_RS_BIN_INSTALL is not unicode: {env:?}")
+        }
+    };
+
+    assert!(!(qiskit_rs_source_install.is_some() && qiskit_rs_binary_install.is_some()), "Only one installation method should be specified");
+
+    if qiskit_rs_source_install.is_some() {
+        return InstallMethod::SOURCE(qiskit_rs_source_install.unwrap());
+    }
+    InstallMethod::BIN(qiskit_rs_binary_install.unwrap())
+}
+
+
+fn build_qiskit_from_source() {
     let curr_dir_str = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let curr_dir: &Path = Path::new(&curr_dir_str);
 
     let qiskit_c_lib = curr_dir.join("qiskit_c_lib");
-    let repo_dir: &Path = qiskit_c_lib.as_path();
-    let repo_dir_str: &str = repo_dir.to_str().unwrap();
+    let qiskit_source_dir: &Path = qiskit_c_lib.as_path();
+
+    let repo_dir_str: &str = qiskit_source_dir
+        .to_str()
+        .expect("Qiskit source directory could not be found");
 
     println!("Cloning qiskit from source into {}", repo_dir_str);
 
     let url = "https://github.com/Qiskit/qiskit.git";
-    let _ = Repository::clone(url, &repo_dir);
+    let _ = match git2::Repository::clone(url, &qiskit_source_dir) {
+        Ok(_) => println!("Repository successfully cloned"),
+        Err(e) => match e.code() {
+            git2::ErrorCode::Exists => {println!("Repository already exists")}
+            _ => panic!("Git clone failed: {e:?}")
+        }
+    };
 
     println!("Generating dynamically linked qiskit libraries");
 
     let _ = Command::new("make")
-        .current_dir(repo_dir)
+        .current_dir(qiskit_source_dir)
         .arg("c")
-        .output()
-        .unwrap();
+        .status()
+        .expect("Dynamically linked library generation failed");
 
-    println!("Dynamically linked libraries generated at {}", repo_dir_str); 
+    println!("Dynamically linked libraries generated at {}", repo_dir_str);
 
     println!("cargo:rustc-env=LD_LIBRARY_PATH={}/dist/c/lib", repo_dir_str);
     println!("cargo:rustc-link-search={}/dist/c/lib", repo_dir_str);
     println!("cargo:rustc-link-lib=qiskit");
+}
+
+
+fn main() { 
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let install_method = check_installation_method();
+
+    match install_method {
+        InstallMethod::SOURCE(_) => {
+            build_qiskit_from_source();
+        },
+        InstallMethod::BIN(_) => {
+            panic!("Binary build not implemented")
+        }
+    }; 
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
