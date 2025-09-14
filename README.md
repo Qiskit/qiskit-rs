71d399e231a5576fb40f7a3f37f96b875364d37d
Emil Kovacev <Emil.Kovacev@ibm.com>
2025-09-14 14:36:17 -0400

    Change build envvars and enable macos build from path

--- README.md
+++ README.md
@@ -5,20 +5,20 @@ Rust bindings for qiskit
 
 ## Installation
 
+qiskit-rs needs the qiskit c api to function. There are two installation methods
+built in to qiskit-rs:
+
+- Path (Manually specified path): Uses qiskit c api binary or source from a path
+    - Set envvar `QISKIT_CEXT_INSTALL_METHOD=path`
+    - Set envvar `QISKIT_CEXT_PATH` to the path of the qiskit source directory
+- Clone (no path specified): Automatically clones and builds the qiskit c api from source. 
+    - Set envvar `QISKIT_CEXT_INSTALL_METHOD=clone` to use the clone method. WARNING, cloning and building from source is very slow.
+
 A crate will be published soon. For now, qiskit-rs can be installed as follows:
 
 ```
 cargo install --path=path/to/qiskit-rs
 ```
-
-qiskit-rs needs the qiskit c api to function. There are two installation methods
-built in to qiskit-rs:
-
-- Path (Manually specified path): Uses qiskit c api binary or source from a path
-    Set envvar `QISKIT_CEXT_PATH` to the path of the qiskit source directory
-- Clone (no path specified): Automatically clones and builds the qiskit c api from source. 
-    Set envvar `QISKIT_CEXT_CLONE=1` to use the clone method. WARNING, cloning and building from source is very slow.
-
 
 ## Getting Started
 
--- build.rs
+++ build.rs
@@ -10,32 +10,32 @@ enum InstallMethod {
 
 // There are two installation methods:
 // - Path (Manually specified path): Uses qiskit c api binary or source from a path
-//     Use envvar QISKIT_CEXT_PATH to specify the path to the qiskit binary
+//     export QISKIT_CEXT_INSTALL_METHOD="path"
+//     export QISKIT_CEXT_PATH="path/to/qiskit-cext-dir"
 // - Clone (no path specified): Automatically clones and builds the qiskit c api from source
-//     Set envvar QISKIT_CEXT_CLONE=1 to use the clone method. WARNING, cloning and building from
+//     Set envvar export QISKIT_CEXT_INSTALL_METHOD="clone" to use the clone method. WARNING, cloning and building from
 //     source is very slow.
 fn check_installation_method() -> InstallMethod {
-    let install_with_path: Option<InstallMethod> = match env::var("QISKIT_CEXT_PATH") {
-        Ok(val) => Some(InstallMethod::PATH(val)),
-        Err(e) => match e {
-            env::VarError::NotPresent => None,
-            env::VarError::NotUnicode(env) => panic!("Envvar QISKIT_CEXT_PATH is not unicode: {env:?}")
+    let qiskit_cext_path = env::var("QISKIT_CEXT_PATH");
+    match env::var("QISKIT_CEXT_INSTALL_METHOD") {
+        Ok(val) => {
+            match val.as_str() {
+                "path" => InstallMethod::PATH(qiskit_cext_path.expect("QISKIT_CEXT_PATH is unset")),
+                "clone" => InstallMethod::CLONE,
+                _ => InstallMethod::PATH(qiskit_cext_path.expect("QISKIT_CEXT_PATH is unset"))
+            }
+        },
+        Err(e) => {
+            match e {
+                env::VarError::NotPresent => {
+                    panic!("Please provide an installation method with the envvar QISKIT_CEXT_INSTALL_METHOD")
+                },
+                env::VarError::NotUnicode(_) => {
+                    panic!("Envvar QISKIT_CEXT_INSTALL_METHOD is not unicode")
+                }
+            }
         }
-    };
-    let install_with_clone: Option<InstallMethod> = match env::var("QISKIT_CEXT_CLONE") {
-        Ok(_) => Some(InstallMethod::CLONE),
-        Err(e) => match e {
-            env::VarError::NotPresent => None,
-            env::VarError::NotUnicode(env) => panic!("Envvar QISKIT_CEXT_CLONE is not unicode: {env:?}")
-        }
-    };
-    if install_with_path.is_some() {
-        return install_with_path.unwrap();
     }
-    else if install_with_clone.is_some() {
-        return install_with_clone.unwrap();
-    }
-    panic!("Please specify an installation method using envvars QISKIT_CEXT_PATH or QISKIT_CEXT_CLONE");
 }
 
 
@@ -113,6 +113,7 @@ fn build_qiskit_from_path(qiskit_path_str: String) {
     }
 
     println!("cargo:rustc-env=LD_LIBRARY_PATH={}/dist/c/lib", qiskit_path_str);
+    println!("cargo:rustc-env=DYLD_LIBRARY_PATH={}/dist/c/lib", qiskit_path_str);
     println!("cargo:rustc-link-search={}/dist/c/lib", qiskit_path_str);
     println!("cargo:rustc-link-lib=qiskit");
 
@@ -131,8 +132,8 @@ fn build_qiskit_from_path(qiskit_path_str: String) {
 
 fn main() { 
     println!("cargo:rerun-if-changed=build.rs");
-    
-    // println!("cargo::rerun-if-env-changed=QISKIT_CEXT_PATH");
+    println!("cargo::rerun-if-env-changed=QISKIT_CEXT_INSTALL_METHOD");
+    println!("cargo::rerun-if-env-changed=QISKIT_CEXT_PATH");
 
     let install_method = check_installation_method();
 
