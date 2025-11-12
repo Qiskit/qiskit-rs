e8890d0fff020584125f9edfb72f92f0dcbcae1e
Matthew Treinish <Matthew.Treinish@ibm.com>
2025-11-12 08:06:11 -0500

    Update qiskit-sys/build.rs

--- qiskit-sys/build.rs
+++ qiskit-sys/build.rs
@@ -8,7 +8,7 @@
 //
 // Any modifications or derivative works of this code must retain this
 // copyright notice, and modified files need to carry a notice indicating
-// that they have been altered from the originals.use std::env;
+// that they have been altered from the originals.
 
 use std::env;
 use std::path::{Path, PathBuf};
